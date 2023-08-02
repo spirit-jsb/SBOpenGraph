7d2801180fe17bad6777ae8912282e36a2a7abf9
Max <sibo_jian_29903549@163.com>
2023-08-02 17:11:32 +0800

    Initial SBOpenGraph project

---
+++ Sources/SBOpenGraph.h
@@ -0,0 +1,18 @@
+//
+//  SBOpenGraph.h
+//  SBOpenGraph
+//
+//  Created by JONO-Jsb on 2023/8/2.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SBOpenGraph.
+FOUNDATION_EXPORT double SBOpenGraphVersionNumber;
+
+//! Project version string for SBOpenGraph.
+FOUNDATION_EXPORT const unsigned char SBOpenGraphVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SBOpenGraph/PublicHeader.h>
+
+
