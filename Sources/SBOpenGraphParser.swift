b5abded95569e527ab80272f651d9926392251df
Max <sibo_jian_29903549@163.com>
2023-08-02 17:32:10 +0800

    Add SBOpenGraphParser.swift file

---
+++ Sources/SBOpenGraphParser.swift
@@ -0,0 +1,14 @@
+//
+//  SBOpenGraphParser.swift
+//  SBOpenGraph
+//
+//  Created by JONO-Jsb on 2023/8/2.
+//
+
+#if canImport(Foundation)
+
+import Foundation
+
+struct SBOpenGraphParser {}
+
+#endif
