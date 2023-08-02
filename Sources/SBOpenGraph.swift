44d4bc27b29fdd10a731001552c854b126a48354
Max <sibo_jian_29903549@163.com>
2023-08-02 17:27:48 +0800

    Add fetch(url:headers:) & fetch(url:headers:completion:) functions in SBOpenGraph.swift

--- Sources/SBOpenGraph.swift
+++ Sources/SBOpenGraph.swift
@@ -5,4 +5,44 @@
 //  Created by JONO-Jsb on 2023/8/2.
 //
 
+#if canImport(Foundation)
+
 import Foundation
+
+public struct SBOpenGraph {
+    #if compiler(>=5.5.2) && canImport(_Concurrency)
+    @available(iOS 13.0, *)
+    public static func fetch(url: URL?, headers: [String: String]? = nil) async throws -> SBOpenGraph {
+        return try await withCheckedThrowingContinuation { continuation in
+            self.fetch(url: url, headers: headers) { result in
+                switch result {
+                    case let .success(successResult):
+                        continuation.resume(returning: successResult)
+                    case let .failure(failureError):
+                        continuation.resume(throwing: failureError)
+                }
+            }
+        }
+    }
+    #elseif compiler(>=5.5) && canImport(_Concurrency)
+    @available(iOS 15.0, *)
+    public static func fetch(url: URL?, headers: [String: String]? = nil) async throws -> SBOpenGraph {
+        return try await withCheckedThrowingContinuation { continuation in
+            self.fetch(url: url, headers: headers) { result in
+                switch result {
+                    case let .success(successResult):
+                        continuation.resume(returning: successResult)
+                    case let .failure(failureError):
+                        continuation.resume(throwing: failureError)
+                }
+            }
+        }
+    }
+    #endif
+
+    public static func fetch(url: URL?, headers: [String: String]? = nil, completion: @escaping (Swift.Result<SBOpenGraph, Error>) -> Void) {
+        fatalError("parse(url:completion:) has not been implemented")
+    }
+}
+
+#endif
