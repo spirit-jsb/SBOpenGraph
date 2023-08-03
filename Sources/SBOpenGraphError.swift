7062174d5396d6f407cdb79b6797c7bb9a5cb20d
Max <sibo_jian_29903549@163.com>
2023-08-03 08:05:45 +0800

    Add SBOpenGraphError.swift file

---
+++ Sources/SBOpenGraphError.swift
@@ -0,0 +1,19 @@
+//
+//  SBOpenGraphError.swift
+//  SBOpenGraph
+//
+//  Created by JONO-Jsb on 2023/8/3.
+//
+
+#if canImport(Foundation)
+
+import Foundation
+
+public enum SBOpenGraphError: Error {
+    case unknown
+    case incompleteContext
+    case unexpectedStatusCode(Int)
+    case encodingError
+}
+
+#endif
