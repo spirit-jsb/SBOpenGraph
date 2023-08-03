516d88f06d7520b37ad91a5d77456d63f28dbcca
Max <sibo_jian_29903549@163.com>
2023-08-03 10:37:31 +0800

    Add return value on fetch(url:headers:completion:) function and implement

--- Sources/SBOpenGraph.swift
+++ Sources/SBOpenGraph.swift
@@ -43,11 +43,44 @@ public struct SBOpenGraph {
     }
     #endif
 
-    public static func fetch(url: URL?, headers: [String: String]? = nil, completion: @escaping (Swift.Result<SBOpenGraph, Error>) -> Void) {
-        fatalError("parse(url:completion:) has not been implemented")
-    }
+    public static func fetch(url: URL, headers: [String: String]? = nil, completion: @escaping (Swift.Result<SBOpenGraph, Error>) -> Void) -> URLSessionDataTask {
+        let urlSession = URLSession(configuration: .default)
 
+        var urlRequest = URLRequest(url: url)
+        if let headers = headers {
+            headers.forEach {
+                urlRequest.setValue($1, forHTTPHeaderField: $0)
+            }
         }
+
+        let task = urlSession.dataTask(with: urlRequest) { data, urlResponse, error in
+            if let error = error {
+                completion(.failure(error))
+            } else {
+                guard let data = data, let urlResponse = urlResponse as? HTTPURLResponse else {
+                    completion(.failure(SBOpenGraphError.unknown))
+                    return
+                }
+
+                if !(200 ..< 300).contains(urlResponse.statusCode) {
+                    completion(.failure(SBOpenGraphError.unexpectedStatusCode(urlResponse.statusCode)))
+                } else {
+                    guard let htmlString = String(data: data, encoding: .utf8) else {
+                        completion(.failure(SBOpenGraphError.encodingError))
+                        return
+                    }
+
+                    let og = SBOpenGraph(htmlString: htmlString)
+
+                    completion(.success(og))
+                }
+            }
+        }
+
+        task.resume()
+
+        return task
+    }
 
     public init(htmlString: String) {
         self.source = SBOpenGraphParser.parser(htmlString: htmlString)
